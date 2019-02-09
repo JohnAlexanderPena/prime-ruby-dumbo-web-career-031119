49140328b4cc497e2e8f93bc997e5a3f18e29220
John Pena <jpena91@gmail.com>
2019-02-09 20:29:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
+require 
 def prime?(num)
   prime_chk = true
   if num < 0 || num == 0 || num == 1 
   prime_chk = false
 end
- (2...num).each do |divisor|
+ (2..num).each do |divisor|
     return false if num % divisor == 0
  end
  prime_chk
